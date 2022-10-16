382e1c9abe98dcbc4a5d0c7d6bd13ccc2304258d
kaitlin <kaitlinfrani@csu.fullerton.edu>
2022-10-16 12:15:57 -0700

    qualification section updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -55,7 +55,29 @@ skillsHeader.forEach((el) =>
 });
 
 /*==================== QUALIFICATION TABS ====================*/
+const tabs=document.querySelectorAll('[data-target]'),
+    tabContents=document.querySelectorAll('[data-content')
 
+tabs.forEach(tab =>
+    {
+        tab.addEventListener('click', function(s)
+        {
+            const target=document.querySelector(tab.dataset.target)
+
+            tabContents.forEach(tabContent =>
+            {
+                tabContent.classList.remove('qualification__active')
+            })
+            target.classList.add('qualification__active')
+
+            tabs.forEach(tab =>
+            {
+                tab.classList.remove('qualification__active')
+            })
+            tab.classList.add('qualification__active')
+
+        })
+    })
 
 /*==================== SERVICES MODAL ====================*/
 
